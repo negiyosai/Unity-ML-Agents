0f7503887d01f48ca862f839f6ee9fa3f77f817a
divinosdev <vivek99.work@gmail.com>
2020-08-17 22:23:00 +0530

    Set scene

---
+++ Assets/Jump.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpHeight = 7f;
+    public bool isGrounded;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                rb.AddForce(Vector3.up * jumpHeight * 100);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            Invoke("BoolChange", 0.1f);
+        }
+    }
+
+    void BoolChange()
+    {
+        isGrounded = true;
+    }
+
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
