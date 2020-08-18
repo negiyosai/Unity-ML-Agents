677ed4fc3d77f2f224abd42f9766b833b8bc8912
divinosdev <vivek99.work@gmail.com>
2020-08-18 23:19:55 +0530

    Working test area with spawner

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("HIT");
+        GameObject player = other.gameObject;
+        GameObject destroyer = other.gameObject;
+
+        if (player.tag == "Player" || destroyer.tag == "Destroyer")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    [SerializeField] float spawnInterval;
+    [SerializeField] GameObject enemy;
+    private void Start()
+    {
+        InvokeRepeating("Spawn", 0, 3);
+    }
+    void Spawn()
+    {
+        Instantiate(enemy, gameObject.transform.position, Quaternion.identity);
+    }
+}
