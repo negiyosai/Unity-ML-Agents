f8182ce46d76ddd906ba529c53159201a238db97
divinosdev <vivek99.work@gmail.com>
2020-08-20 22:58:53 +0530

    removed ml examples, updated jumper with agent class

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -1,24 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using System;
 
-public class Jump : MonoBehaviour
+public class Jump : Agent
 {
     public float jumpHeight = 7f;
     public bool isGrounded;
 
     private Rigidbody rb;
 
-    void Start()
+    public event Action OnReset;
+
+    public override void Initialize()
     {
         rb = GetComponent<Rigidbody>();
+
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        if (Mathf.FloorToInt(vectorAction[0]) == 1)
+            TriggerJump();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        //Reset
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = 0;
+        if (Input.GetKey(KeyCode.Space))
+            actionsOut[0] = 1;
+
+
     }
 
     void Update()
     {
+        //TriggerJump();
+    }
+
+    void TriggerJump()
+    {
         if (isGrounded)
         {
-            if (Input.GetKeyDown("space"))
+            //if (Input.GetKeyDown("space"))
             {
                 rb.AddForce(Vector3.up * jumpHeight * 100);
             }
