4ee7a66fd813e7c5a2d70f9cb0d0417f0e93148d
divinosdev <vivek99.work@gmail.com>
2020-09-07 13:08:42 +0530

    added object spawn counter, changed scene

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -1,24 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    [SerializeField] float spawnInterval;
+    [SerializeField] int spawnIntervalStart;
+    [SerializeField] int spawnIntervalEnd;
     [SerializeField] GameObject enemy;
+    [SerializeField] Text objectSpawnedText;
+    int numberOfObjects = 0;
     bool shouldStart = true;
     private void Start()
     {
         StartCoroutine(StartSpawn());
     }
-
     IEnumerator StartSpawn()
     {
         while (shouldStart)
         {
+            numberOfObjects++;
+            objectSpawnedText.text = "Objects Spawned: " + numberOfObjects.ToString(); 
             Instantiate(enemy, gameObject.transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(Random.Range(3, 5));
+            yield return new WaitForSeconds(Random.Range(spawnIntervalStart, spawnIntervalEnd));
         }
     }
- 
 }
